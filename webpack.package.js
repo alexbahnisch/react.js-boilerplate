26399f847b02897814308ce5ea10d31675f9ef57
Alex Bahnisch <alexbahnisch@gmail.com>
2017-06-21 02:21:52 +1000

    Added style.css to package build.

--- webpack.package.js
+++ webpack.package.js
@@ -3,10 +3,19 @@
 const fs = require("fs");
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 
 
 let packageJson = JSON.parse(fs.readFileSync("./package.json"));
-let name = `${packageJson.name}-${packageJson.version + (process.env.MIN ? ".min" : "")}`;
+let name = `${packageJson.name}-${packageJson.version}${process.env.MIN ? ".min" : ""}`;
+let plugins = [new ExtractTextPlugin(`${name}.css`)];
+
+if (process.env.MIN) {
+  //noinspection JSUnresolvedFunction
+  plugins.push(new webpack.optimize.UglifyJsPlugin())
+} else {
+  plugins.push(new ExtractTextPlugin("styles.css"))
+}
 
 module.exports = {
   entry: path.resolve(__dirname, `./src/package/index.js`),
@@ -30,8 +39,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin(`${name}.css`)
-  ],
-  target: "node"
+  plugins: plugins
 };
