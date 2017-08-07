caf4050a5cb43a302d625fcd8d7fc9d5045c8fd0
Alex Bahnisch <alexbahnisch@gmail.com>
2017-08-08 04:07:11 +1000

    Added flow to project

--- src/app/utils/development.js
+++ src/app/utils/development.js
@@ -1,21 +1,32 @@
 "use strict";
+// @flow
 
-export function addLiveReloadSource() {
-  if (process.env.NODE_ENV !== "production") {
-    let liveReload = document.createElement("script");
-    liveReload.setAttribute("src", "http://localhost:35729/livereload.js");
-    document.body.appendChild(liveReload);
+
+export function appendChild(element: Element): void {
+  if (!!document) {
+    if (!!document.body) {
+      document.body.appendChild(element);
+    }
   }
 }
 
 
-export function createElement(id, tagName = "div") {
+export function addLiveReloadSource(): void {
+  if (process.env.NODE_ENV !== "production") {
+    let liveReload = document.createElement("script");
+    liveReload.setAttribute("src", "http://localhost:35729/livereload.js");
+    appendChild(liveReload)
+  }
+}
+
+
+export function createElement(id: string, tagName: string = "div"): Element {
   let element = document.getElementById(id);
 
   if (!element) {
     element = document.createElement(tagName);
     element.setAttribute("id", id);
-    document.body.appendChild(element);
+    appendChild(element)
   }
 
   return element;
