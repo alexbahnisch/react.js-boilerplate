fe4d20f65ebd7264cce1356e8cdc21be7d2977f1
Alex Bahnisch <alexbahnisch@gmail.com>
2018-11-06 23:09:28 +1000

    Minor refactoring

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDom from "react-dom";
 
 import {App} from "./views/app"
-import {createElement} from "./utils/development"
+import {createElement} from "./utils/index"
 import "./main.css";
 
 
--- src/app/views/app.jsx
+++ src/app/views/app.jsx
@@ -2,7 +2,7 @@
 import React, {Component} from "react"
 import PropTypes from "prop-types"
 
-import {decorator} from "../utils/development";
+import {decorator} from "../utils/index";
 import styles from "./app.css"
 
 
